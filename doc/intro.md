3b9a0acbc1df7e956f7069485a09fad5b4e718ad
wiany11 <wiany11@gmail.com>
2015-02-13 10:04:49 -0600

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to hawk-eyed
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/hawk_eyed/core_test.clj
@@ -0,0 +1,7 @@
+(ns hawk-eyed.core-test
+  (:require [clojure.test :refer :all]
+            [hawk-eyed.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
